c67c85ae1a16a94842b85e7c68cc8e8ec30a5ec2
Aswath1209 <162452125+Aswath1209@users.noreply.github.com>
2025-06-07 18:48:11 +0530

    Update CCGWWE.py

--- CCGWWE.py
+++ CCGWWE.py
@@ -112,7 +112,7 @@ active_games = {}  # chat_id: game_data
 
 # === COMMAND HANDLERS ===
 
-async def star_tmafia(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start_mafia(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
     user = update.effective_user
 
